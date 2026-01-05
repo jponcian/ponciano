7d3ae2aae7f902f36909236469d1046e039621af
Javier <jponciang@gmail.com>
2026-01-05 16:06:13 -0400

    ultimo

--- somossalud/app_somossalud/android/app/src/main/kotlin/com/somossalud/paciente_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.somossalud.paciente_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- somossalud/resources/views/components/application-logo.blade.php
+++
@@ -1,5 +0,0 @@
-<img 
-    src="{{ asset('images/saludsonrisa.jpg') }}" 
-    alt="Salud Sonrisa" 
-    {{ $attributes->class(['application-logo'])->merge(['style' => 'border-radius: 50%; object-fit: cover;']) }}
->
